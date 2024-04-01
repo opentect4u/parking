21fe011b52c4fe3291411ed5a1ec0993e720077b
Sayantika Dhar <sayantika.dhar04@gmail.com>
2024-04-01 11:25:45 +0530

    deliver to server 01.04.2024

--- routes/SuperAdminRouter/CustomerRouter.js
+++ routes/SuperAdminRouter/CustomerRouter.js
@@ -1,9 +1,10 @@
-const { customer } = require('../../controller/superadmin/customer.controller');
+const { customer, customer_edit } = require('../../controller/superadmin/customer.controller');
 const { AuthSuperCheckedMW } = require('../../middleware/AuthChecked.middleware');
 
 const CustomerRouter = require('express').Router();
 
-CustomerRouter.get('/customer',AuthSuperCheckedMW,customer)
+CustomerRouter.get('/customer',AuthSuperCheckedMW,customer);
+CustomerRouter.get('/edit_customer',AuthSuperCheckedMW,customer_edit);
 
 
 module.exports = {CustomerRouter}
