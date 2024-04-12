ce986a7ca6a85074aff735b9c55cc158f7f876ba
Sayantika Dhar <sayantika.dhar04@gmail.com>
2024-04-12 18:44:06 +0530

    doing vehicle rate section 12.04.2024

--- controller/superadmin/vehicleRate.controller.js
+++ controller/superadmin/vehicleRate.controller.js
@@ -1,6 +1,6 @@
 const Joi = require("joi");
 const dateFormat = require("dateformat");
-const { db_Select } = require("../../model/Master.model");
+const { db_Select, db_Insert } = require("../../model/Master.model");
 const { getAllCustomerList } = require("./customer.controller");
 const { getAllSellerList } = require("./seller.controller");
 const { getAllVehicleList } = require("./vehicle.controller");
@@ -16,14 +16,18 @@ const vehicle_rate = async(req,res) =>{
     try {
         var method = req.method
         var selected = {
-          cust_id: method == 'POST' ? req.body.cust_name : ''
+          cust_id: method == 'POST' ? req.body.cust_name : '',
+          veh_name: method == 'POST' ? req.body.veh_id : '',
+          rate_type:  method == 'POST' ? req.body.rate_type : '',
+          veh_id:  method == 'POST' ? req.body.veh_id : '',
         }
+        console.log(selected,'pp');
         var cust = await getAllCustomerList()
         var seller_name = await getAllSellerList()
         var vehicle = await getAllVehicleList()
           veh_rate_list = [];
         if(method == 'POST'){
-          veh_rate_list = await show_vehicle_rate_dtls(selected.cust_id)
+          veh_rate_list = await show_vehicle_rate_dtls(selected.cust_id,selected.veh_name)
           veh_rate_list = veh_rate_list.suc > 0 ? veh_rate_list.msg : []
         }
 
@@ -45,11 +49,11 @@ const vehicle_rate = async(req,res) =>{
       }
 };
 
-const show_vehicle_rate_dtls = (cust_id) => {
+const show_vehicle_rate_dtls = (cust_id,veh_name) => {
     return new Promise(async (resolve, reject) => {
       let select = "a.*,b.*",
         table_name = "md_rate_dtls a, md_seller b",
-        whr = `a.seller_id = b.seller_id AND a.customer_id=${cust_id}`;
+        whr = `a.seller_id = b.seller_id AND a.customer_id=${cust_id} AND a.vehicle_id = ${veh_name}`;
       const vehicle_dt = await db_Select(select, table_name, whr, null);
       resolve(vehicle_dt)
     })
@@ -69,5 +73,43 @@ const show_vehicle_rate_dtls = (cust_id) => {
     });
   };
   
+  const save_add_vehicle_rate = async (req, res) => {
+    try {
+      const schema = Joi.object({
+        id: Joi.required(),
+        sell_id: Joi.required(),
+        cust_name: Joi.optional(),
+        cust_id: Joi.optional(),
+        rate_type: Joi.required(),
+        veh_id: Joi.required(),
+        frm_hr: Joi.required(),
+        to_hr: Joi.required(),
+        park_fee: Joi.required(),
+      });
+      const { error, value } = schema.validate(req.body, { abortEarly: false });
+      console.log(value);
+      if (error) {
+        const errors = {};
+        error.details.forEach((detail) => {
+          errors[detail.context.key] = detail.message;
+        });
+        return res.json({ error: errors });
+      }
+      var user_name = req.session.user.userData.user_name;
+      const datetime = dateFormat(new Date(), "yyyy-mm-dd HH:MM:ss");
+  
+      let fields = value.id > 0 ? `seller_id='${value.sell_id}',rate_type='${value.rate_type}',vehicle_id='${value.veh_id}',from_hour='${value.frm_hr}',to_hour='${value.to_hr}',vehicle_rate='${value.park_fee}',updated_by='${user_name}',updated_at='${datetime}'` : "(seller_id,customer_id,rate_type,vehicle_id,from_hour,to_hour,vehicle_rate,rate_flag,night_day_flag,created_by,created_at)",
+        values = `('${value.sell_id}','${value.cust_name}','${value.rate_type}','${value.veh_id}','${value.frm_hr}','${value.to_hr}','${value.park_fee}','F','O','${user_name}','${datetime}')`;
+      let res_dt = await db_Insert("md_rate_dtls", fields, values, value.id > 0 ? `sl_no=${value.id} AND customer_id = ${value.cust_id}` : null, value.id > 0 ? 1 : 0);
+      console.log("========vehicle==========", res_dt);
+      req.flash("success", value.id > 0 ? "Updated successfully" : "Saved successfully");
+      res.redirect("/superadmin/vehicle_rate");
+    //   res.send(res_dt)
+    } catch (error) {
+      console.log(error);
+      req.flash("error", value.id > 0 ? "Data not updated Successfully" : "Data not saved Successfully");
+      res.redirect("/superadmin/vehicle_rate");
+    }
+  };  
 
-module.exports = {vehicle_rate,show_vehicle_rate_dtls,get_vehicle}
+module.exports = {vehicle_rate,show_vehicle_rate_dtls,get_vehicle,save_add_vehicle_rate}
--- routes/SuperAdminRouter/VehicleRateRouter.js
+++ routes/SuperAdminRouter/VehicleRateRouter.js
@@ -1,11 +1,10 @@
-const { vehicle_rate, get_vehicle } = require('../../controller/superadmin/vehicleRate.controller');
+const { vehicle_rate, get_vehicle, save_add_vehicle_rate } = require('../../controller/superadmin/vehicleRate.controller');
 const { AuthSuperCheckedMW } = require('../../middleware/AuthChecked.middleware');
 
 const VehicleRateRouter = require('express').Router();
 
 VehicleRateRouter.all('/vehicle_rate',AuthSuperCheckedMW,vehicle_rate),
-VehicleRateRouter.post('/vehicle_dt',AuthSuperCheckedMW,get_vehicle)
-
-
+VehicleRateRouter.post('/vehicle_dt',AuthSuperCheckedMW,get_vehicle),
+VehicleRateRouter.post('/vehicle_rate_save',AuthSuperCheckedMW,save_add_vehicle_rate),
 
 module.exports = {VehicleRateRouter}
