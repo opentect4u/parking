bf26a75b49ea4fb0b0f3bb24114bc7cbbed1ca46
Sayantika Dhar <sayantika.dhar04@gmail.com>
2024-04-15 16:03:48 +0530

    delivery vehicle rate 15.04.2024

--- controller/superadmin/customer.controller.js
+++ controller/superadmin/customer.controller.js
@@ -4,12 +4,12 @@ const { db_Select, db_Insert } = require("../../model/Master.model");
 const { getAllSellerList } = require("./seller.controller");
 const { getAllLocationList } = require("./location.controller");
 
-const getAllCustomerList = (id = 0) => {
+const getAllCustomerList = (id = 0, sailer_id = 0) => {
     return new Promise(async (resolve, reject) => {
         var select = "a.*,b.seller_name,b.seller_addr,b.seller_mob,b.location_id,c.*",
         table_name = "md_customer a, md_seller b, md_locations c",
         where = `a.seller_id = b.seller_id
-        AND a.location_id = c.location_id ${id > 0 ? `AND a.customer_id=${id}` : ''}`,
+        AND a.location_id = c.location_id ${id > 0 ? `AND a.customer_id=${id}` : ''} ${sailer_id > 0 ? `AND a.seller_id=${sailer_id}` : ''}`,
         order = `Order By a.seller_id ASC`;
         var cust = await db_Select(select,table_name,where,order);
         resolve(cust)
--- routes/SuperAdminRouter/VehicleRateRouter.js
+++ routes/SuperAdminRouter/VehicleRateRouter.js
@@ -1,4 +1,4 @@
-const { vehicle_rate, get_vehicle, save_add_vehicle_rate } = require('../../controller/superadmin/vehicleRate.controller');
+const { vehicle_rate, get_vehicle, save_add_vehicle_rate, getCustListAjax, vehicle_rate_edit } = require('../../controller/superadmin/vehicleRate.controller');
 const { AuthSuperCheckedMW } = require('../../middleware/AuthChecked.middleware');
 
 const VehicleRateRouter = require('express').Router();
@@ -6,5 +6,7 @@ const VehicleRateRouter = require('express').Router();
 VehicleRateRouter.all('/vehicle_rate',AuthSuperCheckedMW,vehicle_rate),
 VehicleRateRouter.post('/vehicle_dt',AuthSuperCheckedMW,get_vehicle),
 VehicleRateRouter.post('/vehicle_rate_save',AuthSuperCheckedMW,save_add_vehicle_rate),
+VehicleRateRouter.get('/vehicle_edit_rate',AuthSuperCheckedMW,vehicle_rate_edit),
+VehicleRateRouter.post('/cust_list_ajax',AuthSuperCheckedMW,getCustListAjax),
 
 module.exports = {VehicleRateRouter}
