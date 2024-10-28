153d3f4f43505d2f97ed61dd4e84a8d2b8153ea3
Utsab Roy <roy.utsab01@gmail.com>
2024-10-28 18:23:15 +0530

    add time in report__281024

--- src/routes/config.js
+++ src/routes/config.js
@@ -1,6 +1,6 @@
 // export const BASE_URL = "http://192.168.1.240:3001/api"
 // export const BASE_URL = "http://192.168.1.240:3001/api"
 // export const BASE_URL = "http://192.168.1.252:3001/api"
-// export const BASE_URL_V2 = "https://myparking.opentech4u.co.in/v2/api";
+export const BASE_URL_V4 = "https://myparking.opentech4u.co.in/v4/api";
 // export const BASE_URL = "http://192.168.1.252:3001/v1/api";
-export const BASE_URL_V2 = "http://192.168.1.239:3001/v2/api";
+// export const BASE_URL_V2 = "http://192.168.1.252:3001/v2/api";
